2c6bf35209c00762b83fd7d572c292b7964cfef0
githzchen <59717746+githzchen@users.noreply.github.com>
2022-05-12 14:50:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+rm -rf feeds/luci/themes/luci-theme-argon
 chmod 0755 files/etc/init.d/softethervpnclient
 chmod 0755 files/etc/init.d/tproxy
 chmod 0755 files/etc/init.d/redsocks2
